0e737e214f589fff44917616849172e2e490acbe
Tejashwini1204 <tejashwiniy591@gmail.com>
2026-05-10 12:42:10 +0530

    Add files via upload

--- team-task-manager/backend/main.py
+++ team-task-manager/backend/main.py
@@ -2,24 +2,28 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from routes.auth import router as auth_router
-from routes.projects import router as project_router
 from routes.tasks import router as task_router
-
+from routes.projects import router as project_router
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:5173",
+    "https://team-task-manager-seven-peach-42.vercel.app"
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 app.include_router(auth_router)
-app.include_router(project_router)
 app.include_router(task_router)
+app.include_router(project_router)
 
 @app.get("/")
 def home():
-    return {"message": "Team Task Manager API Running"}
+    return {"message": "Backend Running"}
